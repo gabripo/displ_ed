2c6197751fd28dce5f5f224435857fe4d95d238e
gabripo <gabripo93+github@gmail.com>
2023-08-24 21:59:55 +0200

    Clean and deactivate functionalities

--- src/wall_led.cpp
+++ src/wall_led.cpp
@@ -1,23 +1,9 @@
 #include <Arduino.h>
-/// @file    DemoReel100.ino
-/// @brief   FastLED "100 lines of code" demo reel, showing off some effects
-/// @example DemoReel100.ino
-
 #include <FastLED.h>
 
 FASTLED_USING_NAMESPACE
 
-// FastLED "100-lines-of-code" demo reel, showing just a few
-// of the kinds of animation patterns you can quickly and easily
-// compose using FastLED.
-//
-// This example also shows one easy way to define multiple
-// animations patterns and have them automatically rotate.
-//
-// -Mark Kriegsman, December 2014
-
 #define DATA_PIN 3
-// #define CLK_PIN   4
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
 #define NUM_LEDS 300
@@ -38,11 +24,8 @@ void juggle();
 void setup() {
     delay(3000);  // 3 second delay for recovery
 
-    // tell FastLED about the LED strip configuration
     FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-    // FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 
-    // set master brightness control
     FastLED.setBrightness(BRIGHTNESS);
 }
 
@@ -55,16 +38,15 @@ uint8_t gHue = 0;                   // rotating "base color" used by many of the
 
 void loop() {
     // Call the current pattern function once, updating the 'leds' array
-    gPatterns[gCurrentPatternNumber]();
+    // gPatterns[gCurrentPatternNumber]();
 
     // send the 'leds' array out to the actual LED strip
     FastLED.show();
     // insert a delay to keep the framerate modest
-    FastLED.delay(1000 / FRAMES_PER_SECOND);
+    // FastLED.delay(1000 / FRAMES_PER_SECOND);
 
-    // do some periodic updates
-    EVERY_N_MILLISECONDS(20) { gHue++; }    // slowly cycle the "base color" through the rainbow
-    EVERY_N_SECONDS(10) { nextPattern(); }  // change patterns periodically
+    // EVERY_N_MILLISECONDS(20) { gHue++; }    // slowly cycle the "base color" through the rainbow
+    // EVERY_N_SECONDS(10) { nextPattern(); }  // change patterns periodically
 }
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
