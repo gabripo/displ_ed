be975125cb641e7f136a937322c8eb5fc3c83ca8
gabripo <gabripo93+github@gmail.com>
2023-08-29 19:57:43 +0200

    Hard set of initial isMotion value

--- include/motion_sensor.cpp
+++ include/motion_sensor.cpp
@@ -1,6 +1,7 @@
 #include "motion_sensor.h"
 
 void setup_motion_sensor() {
+    isMotion = 0;
     pinMode(interruptPin, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(interruptPin), motion_event, HIGH);  // Sensor in Repeatable Trigger mode!
 }
