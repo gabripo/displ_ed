0c4eaf48e137eed828a1cb337ec0efe4fc1e4d46
gabripo <gabripo93+github@gmail.com>
2023-08-29 21:39:25 +0200

    Invertion functionality for img to hex converter

--- utils/convert_img_to_hex.py
+++ utils/convert_img_to_hex.py
@@ -1,12 +1,27 @@
 import sys
 from PIL import Image
 
-def convert_image_to_hex(image_path):
+def invert_even_rows(image):
+    width, height = image.size
+    pixels = image.load()
+
+    for y in range(height):
+        if y % 2 == 1:  # Check if row is even (rows are 0-indexed)
+            for x in range(width):
+                r, g, b = pixels[x, y]
+                inverted_color = (255 - r, 255 - g, 255 - b)
+                pixels[x, y] = inverted_color
+
+def convert_image_to_hex(image_path, invert):
     # Open the image
     image = Image.open(image_path)
 
     # Convert the image to RGB mode
     image = image.convert('RGB')
+
+    # Invert even rows if specified
+    if invert:
+        invert_even_rows(image)
 
     # Get the pixel data
     pixels = list(image.getdata())
@@ -16,7 +31,7 @@ def convert_image_to_hex(image_path):
     for i, pixel in enumerate(pixels, start=1):
         r, g, b = pixel
         rgb_hex = (r << 16) | (g << 8) | b
-        hex_values.append(format(rgb_hex, '06x'))
+        hex_values.append('0x'+ format(rgb_hex, '06x') + ',')
 
         if i % 16 == 0:  # Add a newline after every 16 values
             hex_values.append('\n')
@@ -30,9 +45,10 @@ def convert_image_to_hex(image_path):
         output_file.write(hex_string)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <image_path>")
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <image_path> [--invert]")
         sys.exit(1)
 
     image_path = sys.argv[1]
-    convert_image_to_hex(image_path)
+    invert = '--invert' in sys.argv
+    convert_image_to_hex(image_path, invert)
