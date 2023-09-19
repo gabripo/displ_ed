2526a2984b662b097310e039f0ab72bde62e4a82
gabripo <gabripo93+github@gmail.com>
2023-09-19 22:02:50 +0200

    ISR if falling or rising edge

--- include/motion_sensor.cpp
+++ include/motion_sensor.cpp
@@ -3,7 +3,7 @@
 void setup_motion_sensor() {
     isMotion = 0;
     pinMode(interruptPin, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(interruptPin), motion_event, HIGH);  // Sensor in Repeatable Trigger mode!
+    attachInterrupt(digitalPinToInterrupt(interruptPin), motion_event, CHANGE);  // Sensor in Repeatable Trigger mode!
 }
 
 void motion_event() {
