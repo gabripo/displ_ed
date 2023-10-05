b68f8f29d50c2ed2b5a7e9cfb2b0217e85376246
gabripo <gabripo93+github@gmail.com>
2023-10-05 11:20:38 +0200

    Inversion is not of color but simple flip in row

--- utils/convert_img_to_hex.py
+++ utils/convert_img_to_hex.py
@@ -1,23 +1,24 @@
 import sys
 from PIL import Image
 
+
 def invert_even_rows(image):
     width, height = image.size
     pixels = image.load()
 
-    for y in range(height):
-        if y % 2 == 1:  # Check if row is even (rows are 0-indexed)
-            for x in range(width):
-                r, g, b = pixels[x, y]
-                inverted_color = (255 - r, 255 - g, 255 - b)
-                pixels[x, y] = inverted_color
+    even_rows = [y for y in range(height) if y % 2 == 0]
+    for y in even_rows:
+        row_pixels = [pixels[x, y] for x in range(width)]
+        for x in range(width):
+            pixels[x, y] = row_pixels[width - 1 - x]
+
 
 def convert_image_to_hex(image_path, invert):
     # Open the image
     image = Image.open(image_path)
 
     # Convert the image to RGB mode
-    image = image.convert('RGB')
+    image = image.convert("RGB")
 
     # Invert even rows if specified
     if invert:
@@ -31,24 +32,25 @@ def convert_image_to_hex(image_path, invert):
     for i, pixel in enumerate(pixels, start=1):
         r, g, b = pixel
         rgb_hex = (r << 16) | (g << 8) | b
-        hex_values.append('0x'+ format(rgb_hex, '06x') + ',')
+        hex_values.append("0x" + format(rgb_hex, "06x") + ",")
 
         if i % 16 == 0:  # Add a newline after every 16 values
-            hex_values.append('\n')
+            hex_values.append("\n")
 
     # Join the hexadecimal values with spaces
-    hex_string = ' '.join(hex_values)
+    hex_string = " ".join(hex_values)
 
     # Write the hex string to the output file
-    output_file_path = 'output.txt'
-    with open(output_file_path, 'w') as output_file:
+    output_file_path = "output.txt"
+    with open(output_file_path, "w") as output_file:
         output_file.write(hex_string)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python script.py <image_path> [--invert]")
         sys.exit(1)
 
     image_path = sys.argv[1]
-    invert = '--invert' in sys.argv
+    invert = "--invert" in sys.argv
     convert_image_to_hex(image_path, invert)
